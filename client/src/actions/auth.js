7db011d3307ce3fc2f68dab2ab91fcd7050c19f2
wulman16 <will.ulman@gmail.com>
2019-07-08 10:44:09 -0400

    Implement loadUser action and setAuthToken helper

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,32 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR
+} from "./types";
+import setAuthToken from "../utils/setAuthToken";
+
+// Load User
+export const loadUser = () => async dispatch => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get("/api/auth");
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR
+    });
+  }
+};
 
 // Register User
 export const register = ({
@@ -18,7 +44,7 @@ export const register = ({
   const body = JSON.stringify({ name, location, email, password });
 
   try {
-    const res = await axios.post("api/users", body, config);
+    const res = await axios.post("/api/users", body, config);
 
     dispatch({
       type: REGISTER_SUCCESS,
